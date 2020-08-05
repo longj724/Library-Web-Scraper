26da8433a5ab80ee6f48e4c1d991fea370449c58
juslong <juslong@umich.edu>
2020-08-04 23:26:55 -0400

    Displays user's books and add book to database

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+import AddIcon from '@material-ui/icons/Add';
 import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
@@ -11,6 +13,7 @@ const useStyles = makeStyles((theme) => ({
         width: '100vw',
         height: '100vh',
         position: 'relative',
+        overflowY: 'auto',
     },
     form: {
         position: 'absolute',
@@ -35,12 +38,37 @@ const useStyles = makeStyles((theme) => ({
         fontSize: 30,
     },
     bookDiv: {
-      width: '20vw',
-      height: '70vh',
-      marginTop: '30vh',
-      marginLeft: '40vw',
-      position: 'absolute'
-    }
+        width: '20vw',
+        height: '70vh',
+        marginTop: '30vh',
+        marginLeft: '40vw',
+        position: 'absolute',
+    },
+    listElement: {
+        color: '#fff',
+        marginBottom: '1vh',
+        marginTop: '1vh',
+    },
+    addIcon: {
+        fontSize: 10,
+    },
+    addIconButton: {
+        color: '#000',
+        backgroundColor: '#fff',
+        '&:hover': {
+            backgroundColor: '#FEC524',
+        },
+        marginLeft: '10px',
+        display: 'inline',
+        marginBottom: '1vh',
+    },
+    myBook: {
+        marginTop: '120px',
+        marginLeft: '45vw',
+        position: 'absolute',
+        backgroundColor: '#FEC524',
+        width: '10vw',
+    },
 }));
 
 function App() {
@@ -48,28 +76,61 @@ function App() {
 
     const [book, setBook] = useState('');
     const [searchResults, setSearchResults] = useState([]);
+    const [myBooks, setMyBooks] = useState([]);
 
     const handleChange = (event) => {
         setBook(event.target.value);
     };
 
     const handleSubmit = () => {
+        setMyBooks([]);
         axios
             .post('/search-book', { book: book })
             .then((res) => {
                 return res.data;
             })
             .then((data) => {
-                setSearchResults(data);
-                console.log(data);
+                data.length !== 0
+                    ? setSearchResults(data)
+                    : setSearchResults([0]);
+            });
+    };
+
+    const addBook = (event) => {
+        let bookIndex = parseInt(event.target.value);
+        axios
+            .post('/add-book', { book: searchResults[bookIndex] })
+            .then((res) => {
+                console.log(res);
+            });
+    };
+
+    const getMyBooks = () => {
+        setSearchResults([]);
+        axios
+            .get('/my-books')
+            .then((res) => {
+                return res.data;
+            })
+            .then((data) => {
+                data.length !== 0
+                    ? setMyBooks(data)
+                    : setMyBooks([0]);
             });
     };
 
     return (
         <div className={classes.root}>
+            <Button
+                variant="contained"
+                className={classes.myBook}
+                onClick={() => getMyBooks()}
+            >
+                My Books
+            </Button>
             <form className={classes.form}>
                 <TextField
-                    label="Book"
+                    label="Search Library for Book"
                     variant="filled"
                     className={classes.textField}
                     onChange={(e) => handleChange(e)}
@@ -82,19 +143,53 @@ function App() {
                 </IconButton>
             </form>
             <div className={classes.bookDiv}>
-                {searchResults.map((book) => {
-                    return (
-                        <li style={{ color: '#fff'}}>
-                            {book[0] +
-                                ' ' +
-                                book[1] +
-                                ' ' +
-                                book[2] +
-                                ', Status: ' +
-                                book[3]}
-                        </li>
-                    );
-                })}
+                {searchResults[0] !== 0 ? (
+                    searchResults.map((book, index) => {
+                        return (
+                            <div key={index}>
+                                <li className={classes.listElement}>
+                                    {book[0] +
+                                        ' ' +
+                                        book[1] +
+                                        ' ' +
+                                        book[2] +
+                                        ', Status: ' +
+                                        book[3]}
+                                </li>
+                                <IconButton
+                                    value={index.toString()}
+                                    className={classes.searchIconButton}
+                                    onClick={(e) => addBook(e)}
+                                >
+                                    <AddIcon className={classes.addIcon} />
+                                </IconButton>
+                            </div>
+                        );
+                    })
+                ) : (
+                    <li className={classes.listElement}>No books found</li>
+                )}
+                {myBooks[0] !== 0 ? (
+                    myBooks.map((book, index) => {
+                        return (
+                            <div key={index}>
+                                <li className={classes.listElement}>
+                                    {book['title'] +
+                                        ' ' +
+                                        book['author'] +
+                                        ' ' +
+                                        book['type'] +
+                                        ', Status: ' +
+                                        book['status']}
+                                </li>
+                            </div>
+                        );
+                    })
+                ) : (
+                    <li className={classes.listElement}>
+                        You have no books saved
+                    </li>
+                )}
             </div>
         </div>
     );
