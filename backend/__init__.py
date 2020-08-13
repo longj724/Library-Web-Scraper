ae69563887f7f1de491962ec59fbe4c76b8900bc
juslong <juslong@umich.edu>
2020-08-12 21:03:45 -0400

    Checking for updated book statuses

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from twilio.rest import Client
+from apscheduler.schedulers.background import BackgroundScheduler
 
 def create_app(test_config=None):
     # create and configure the app
@@ -34,7 +36,7 @@ def create_app(test_config=None):
     def search_for_book():
         if request.method == 'POST':
             title = request.get_json()
-            books = check_availability(title['book'])
+            books = search_library(title['book'])
             
             book_dict = {}
             for i in range(len(books)):
@@ -64,7 +66,6 @@ def create_app(test_config=None):
         try:
             db = get_db()
             book = request.get_json()
-            print(book['book'])
 
             db.execute('INSERT INTO books (title, author, kind, available) VALUES (:title, :author, :kind, :available)', book['book'])
 
@@ -77,9 +78,58 @@ def create_app(test_config=None):
         finally:
             return msg
 
+    def get_statuses():
+        PATH = '/Users/justinlong/Documents/chromedriver'
+        options = webdriver.ChromeOptions()
+        options.add_argument('--incognito')
+        options.add_argument('--headless')
+        options.add_argument('--disable-extensions')
+        options.add_argument('start-maximized')
+        options.add_argument('disable-infobars')
+        driver = webdriver.Chrome(chrome_options=options, executable_path=PATH)
+
+        from backend.my_books import get_db
+        db = get_db()
+
+        try:
+            db_titles = db.execute('SELECT * from books').fetchall()
+        except Exception as ex: 
+            print(ex)
+
+        statuses = []
+        titles = []
+        authors = []
+        versions = []
+
+        for book in db_titles:
+            driver.get('https://ccpl.overdrive.com/search?query=' + book[1])
+
+            element = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'title-header-bar'))
+            )
+            statuses = driver.find_elements_by_class_name('title-header-bar')
+            titles = driver.find_elements_by_class_name('title-name')
+            authors = driver.find_elements_by_class_name('title-author')
+            versions = driver.find_elements_by_class_name('title-format-badge')
+
+            for i in range(len(titles)):
+                if titles[i].text == book[1] and authors[i].text == book[2] and versions[i].text == book[3]:
+                    if statuses[i].text != book[4]:
+                        db.execute('UPDATE books SET available=? WHERE title=?', (statuses[i].text, book[1]))
+                        db.commit()
+            
+            driver.quit()
+    
+    with app.app_context():
+        # get_statuses()
+        sched = BackgroundScheduler(daemon=True)
+        sched.add_job(lambda: print('working'), 'cron', day_of_week='mon,wed,fri')
+        sched.start()
+
+
     return app
 
-def check_availability(title):
+def search_library(title):
     PATH = '/Users/justinlong/Documents/chromedriver'
     options = webdriver.ChromeOptions()
     options.add_argument('--incognito')
